9c22bcfa6529f5bff69d4b81aa030d6d48a39e95
AKINFOLA <110199542+Akinfola@users.noreply.github.com>
2023-03-12 13:40:59 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function myDetails(){
+    let name="TO MY PAGE"
+    let click="CLICK ON OK TO CONTINUE SURFING"
+
+    const message="WELCOME"+" "+name +"\n"+"" + click;
+    alert(message)};
+    myDetails()
